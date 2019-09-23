48678f80d9390b530cb97966db492cf01d1c4a43
Davidyuk Igor <i.davidyuk@expasoft.tech>
2019-09-23 20:35:56 +0700

    creators added for mask rcnn

--- MASKRCNN_creator.py
+++ MASKRCNN_creator.py
@@ -41,6 +41,51 @@ def maskrcnn_mobileNetV3_fpn(num_classes=3, backbone_chkp=False, **kwargs):
     '''
     This function builds torchvision version of MASK RCNN using FPN-mobileNetV3 backbone.
     '''
+
     from torchvision.models.detection.mask_rcnn import MaskRCNN
     backbone = mobilenetV3_fpn_backbone(backbone_chkp)
     return MaskRCNN(backbone, num_classes, **kwargs)
+
+def keypointmaskrcnn_mobileNetV3_fpn(num_classes=3, num_keypoints=4, backbone_chkp=False, **kwargs):
+    '''
+    This function builds torchvision version of MASK RCNN using FPN-mobileNetV3 backbone.
+    '''
+    print(num_keypoints)
+    from torchvision.models.detection.keypoint_rcnn import KeypointRCNN
+    backbone = mobilenetV3_fpn_backbone(backbone_chkp)
+    return KeypointRCNN(backbone, num_classes, num_keypoints=num_keypoints, **kwargs)
+
+def kp_mask_rcnn_mobileNetV3_fpn(num_classes=3, num_keypoints=4, backbone_chkp=False, **kwargs):
+    '''
+    This function builds torchvision version of MASK RCNN using FPN-mobileNetV3 backbone.
+    '''
+    print(num_keypoints)
+    from torchvision.models.detection.keypoint_rcnn import KeypointRCNN
+    backbone = mobilenetV3_fpn_backbone(backbone_chkp)
+    model = KeypointRCNN(backbone, num_classes, num_keypoints=num_keypoints, **kwargs)
+
+    out_channels = model.backbone.out_channels
+
+    from torchvision.models.detection.mask_rcnn import MaskRCNNHeads, MaskRCNNPredictor
+    from torchvision.ops import MultiScaleRoIAlign
+    mask_roi_pool = MultiScaleRoIAlign(
+        featmap_names=[0, 1, 2, 3],
+        output_size=14,
+        sampling_ratio=2)
+
+    mask_layers = (256, 256, 256, 256)
+    mask_dilation = 1
+    mask_head = MaskRCNNHeads(out_channels, mask_layers, mask_dilation)
+
+    mask_predictor_in_channels = 256  # == mask_layers[-1]
+    mask_dim_reduced = 256
+    mask_predictor = MaskRCNNPredictor(mask_predictor_in_channels,
+                                        mask_dim_reduced, num_classes)
+
+    model.roi_heads.mask_roi_pool = mask_roi_pool
+    model.roi_heads.mask_head = mask_head
+    model.roi_heads.mask_predictor = mask_predictor
+
+    return model
+
+
